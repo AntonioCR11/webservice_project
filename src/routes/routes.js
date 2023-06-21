66d141ef701d4e31a4b533588c7974915c3c8ac0
DraSelXL <uni.nathanwill02@gmail.com>
2023-06-21 18:00:54 +0700

    Remove an unfinished git merge syntax

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,11 +9,8 @@ const {
 
 // Import the dev users route
 const devUserRoutes = require(path.join(__dirname, "dev_user_routes"));
-<<<<<<< HEAD
 const commentRoutes = require(path.join(__dirname, "comment_routes"));
-=======
 const UserRoutes = require(path.join(__dirname, "user_routes"));
->>>>>>> dc2d51d5184c9fb8fa005d1cd89c9b1433dcb3d9
 
 router.get('/exampleRoute', exampleFunction);
 
